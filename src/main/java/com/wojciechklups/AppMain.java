ee495ad9304b7b906e10f915769210936ec1dd55
WojciechKlups <wojciech.klups@gmail.com>
2023-02-27 21:09:42 +0100

    Added main class and pom versions update

---
+++ src/main/java/com/wojciechklups/AppMain.java
@@ -0,0 +1,24 @@
+/***********************************************************
+ *
+ * Copyright (c) 2022 Wojciech Klupś
+ *
+ * All rights reserved
+ *
+ ************************************************************/
+package com.wojciechklups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Author: wklups
+ * @timestamp Date: 2023-02-27 21:02:00 +0200 (27 lut 2023)
+ */
+@SpringBootApplication
+public class AppMain
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AppMain.class, args);
+    }
+}
